50445b7e49d047521b93aaa3979dcc3e63600dbd
mnokno <kubaalfaproCode@gmail.com>
2022-04-27 22:05:01 +0100

    More changes

--- app/src/main/java/com/example/wordlemaster/MainActivity.kt
+++ app/src/main/java/com/example/wordlemaster/MainActivity.kt
@@ -8,23 +8,30 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.LinearLayout
+import android.widget.ScrollView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.marginLeft
 import androidx.core.view.setMargins
+import androidx.core.view.size
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
 import kotlin.math.floor
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         genUI()
+
     }
 
     private fun genUI(){
 
         // Gets screen size
+        val constraintLayout = findViewById<LinearLayout>(R.id.mainStackPanel)
+
         val displayMetrics = DisplayMetrics()
         windowManager.defaultDisplay.getMetrics(displayMetrics)
         val linearLayoutPaddingLength: Int = floor(displayMetrics.widthPixels * 0.075).toInt()
@@ -34,9 +41,9 @@ class MainActivity : AppCompatActivity() {
         // Generates horizontal layouts for button
         for (i in 0 until 6){
 
-            val constraintLayout = findViewById<LinearLayout>(R.id.mainStackPanel)
+
             val linearLayout = LinearLayout(this)
-            linearLayout.layoutParams = ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT)
+            linearLayout.layoutParams = LinearLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT)
             linearLayout.setPadding(linearLayoutPaddingLength, buttonPaddingLength, linearLayoutPaddingLength, buttonPaddingLength)
             linearLayout.orientation = LinearLayout.HORIZONTAL
             constraintLayout.addView(linearLayout);
@@ -48,8 +55,7 @@ class MainActivity : AppCompatActivity() {
                 val layoutParams = LinearLayout.LayoutParams(buttonSize, buttonSize)
                 layoutParams.setMargins(buttonPaddingLength, 0, buttonPaddingLength, 0)
                 button.layoutParams = layoutParams
-
-                button.text = "B"
+                button.text = "${constraintLayout.width}"
                 button.setOnClickListener(View.OnClickListener {
                     button.text = button.text as String + '.'
                 })
@@ -60,4 +66,10 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    fun sendMessage(view: View) {
+        // Do something in response to button
+        (view as Button).text = "${findViewById<LinearLayout>(R.id.mainStackPanel).width}"
+    }
+
 }
